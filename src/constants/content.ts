37f12347fbccbd809092fea0b9b6f9aa189998bc
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-04-03 08:16:49 +0900

    feat: Add FormattedMarkdownTableButton component.

--- src/constants/content.ts
+++ src/constants/content.ts
@@ -1,4 +1,9 @@
 /**
+ * Contents Script Config
+ */
+export const CS_MATCHES = ["https://gitlab.com/*"]
+
+/**
  * Format Button
  */
 export const FORMAT_BUTTON = {
---
+++ src/contents/FormattedMarkdownTableButton.tsx
@@ -0,0 +1,45 @@
+import type { PlasmoCSConfig, PlasmoGetInlineAnchor } from "plasmo"
+
+import { CS_MATCHES, FORMAT_BUTTON } from "~constants"
+import { getIssuableFormElement } from "~utils"
+
+// Configuration for PlasmoCS
+export const config: PlasmoCSConfig = {
+  matches: CS_MATCHES
+}
+
+// Function to get the inline anchor
+export const getInlineAnchor: PlasmoGetInlineAnchor = () => {
+  // Returns the anchor element for the format button
+  return document.querySelector(FORMAT_BUTTON.anchor)
+}
+
+// Function to get the shadow host ID
+export const getShadowHostId = () => {
+  // Returns the host ID for the format button
+  return FORMAT_BUTTON.hostId
+}
+
+// Component to format markdown table
+const FormattedMarkdownTableButton = () => {
+  // Function to format and overwrite the markdown table
+  const formatAndOverwrite = () => {
+    // Get the text field element
+    const textField = getIssuableFormElement()
+    console.debug(textField.value)
+  }
+  // Return the button element
+  return (
+    <button
+      style={{
+        borderRadius: 4,
+        padding: 4
+      }}
+      onClick={formatAndOverwrite}>
+      Format Table
+    </button>
+  )
+}
+
+// Export the FormattedMarkdownTableButton component as default
+export default FormattedMarkdownTableButton
